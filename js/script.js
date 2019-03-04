8f856b95c44567cf5fabacc87b77f6d5253d3dfc
joshua <javanmega0@gmail.com>
2019-03-04 07:54:45 +0300

    addition of read me

--- js/script.js
+++ js/script.js
@@ -5,10 +5,11 @@ var playing = function() {
   return Math.floor(6 * Math.random()) + 1;
 }
 
-function gamer(turn) {
+function gamer() {
   this.play = 0;
   this.finalscore = 0;
   this.total = 0;
+  this.turn;
   this.playerIdentity;
 }
 
@@ -24,7 +25,7 @@ gamer.prototype.roll = function() {
 
 gamer.prototype.pause = function() {
   this.total += this.finalscore;
-  this.tempscore = 0;
+  this.finalscore = 0;
   alert(this.playerIdentity + "NEXT PLAYER");
 }
 gamer.prototype.number1Confirm = function() {
@@ -41,17 +42,17 @@ gamer.prototype.freshGame = function() {
 }
 
 var renew = function() {
-  $("gamername1").val("");
-  $("gamername2").val("");
+  $(".gamername1").val("");
+  $(".gamername2").val("");
 }
 
 $(document).ready(function() {
 
   $("button#button00").click(function(event) {
-    gamer00 = new gamer(true);
-    gamer01 = new gamer(false);
-    $(".back-page").show();
-    $(".front-page").hide();
+    player01 = new gamer(true);
+    player02 = new gamer(false);
+    $("").show();
+    $("").hide();
 
     var gamername1 = $(".gamername1").val();
     $("#gamername1").text(gamername1);
@@ -59,15 +60,15 @@ $(document).ready(function() {
     var gamername2 = $(".gamername2").val();
     $("#gamername2").text(gamername2);
 
-    gamer00.gamername = gamername1;
-    gamer01.gamername = gamername2;
+    player01.gamername = gamername1;
+    player02.gamername = gamername2;
 
   });
   $("button#button1").click(function(event) {
-    $(".back-page").hide();
+    $("").hide();
     clearValues();
-    gamer00.freshGame();
-    gamer01.freshGame();
+    player01.freshGame();
+    player02.freshGame();
     $("#round00").empty();
     $("#total001").empty();
     $("#rolls001").empty();
@@ -75,37 +76,37 @@ $(document).ready(function() {
     $("#total002").empty();
     $("#rolls002").empty();
 
-    $(".front-page").show();
+    $("").show();
   });
 
   $("button#player001").click(function(event) {
-    gamer00.play = playing();
-    $("#rolls001").text(gamer1.play);
-    gamer00.rollone();
-    $("#round00").text(gamer1.finalscore);
+    player01.play = playing();
+    $("#rolls001").text(player01.play);
+    player02.rollone();
+    $("#round00").text(player01.finalscore);
   });
 
   $("button#player002").click(function(event) {
-    gamer01.play = playing();
-    $("#rolls002").text(gamer2.play);
-    gamer01.rollone();
-    $("#round001").text(gamer2.finalscore);
+    player02.play = playing();
+    $("#rolls002").text(player02.play);
+    player02.rollone();
+    $("#round001").text(player02.finalscore);
   });
 
   $("button#player011").click(function(event) {
-    gamer00.hold();
-    $("#total001").text(gamer1.total);
+    player01.pause();
+    $("#total001").text(player01.total);
     $("#round00").empty();
     $("#rolls001").empty();
-    gamer00.number1Confirm();
+    player01.number1Confirm();
   });
 
   $("button#player022").click(function(event) {
-    gamer01.pause();
-    $("#total002").text(gamer2.total);
+    player02.pause();
+    $("#total002").text(player02.total);
     $("#round001").empty();
     $("#rolls002").empty();
-    gamer01.number1Confirm();
+    player02.number1Confirm();
   });
 
 });
